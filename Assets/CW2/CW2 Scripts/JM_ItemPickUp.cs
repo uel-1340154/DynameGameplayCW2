570a8fdc81ef84cb80c86591ca3844c97ae0ece0
uel-1340154 <u1340154@uel.ac.uk>
2016-01-02 01:16:51 +0000

    Second commit

--- Assets/CW2/CW2 Scripts/JM_ItemPickUp.cs
+++ Assets/CW2/CW2 Scripts/JM_ItemPickUp.cs
@@ -46,43 +46,36 @@ public class JM_ItemPickUp : MonoBehaviour
         Rarity = Random.Range(0, 950);
         if(Rarity >= 0 && Rarity <= 350)
         {
-            Debug.Log("Item is common!");
             itemParticles[0].startColor = new Color(192, 192, 192);//grey
             //code to change associated particle effect colours
         }
         else if(Rarity >= 351 && Rarity <= 600)
         {
-            Debug.Log("Item is uncommon!");
             itemParticles[0].startColor = new Color(0, 204, 0);//green
             //code to change associated particle effect colours
         }
         else if(Rarity >= 601 && Rarity <= 750)
         {
-            Debug.Log("Item is Rare!");
             itemParticles[0].startColor = new Color(0, 0, 255);//blue
             //code to change associated particle effect colours
         }
         else if(Rarity >= 751 && Rarity <= 800)
         {
-            Debug.Log("Item as Fantasical!");
             itemParticles[0].startColor = new Color(102, 0, 204);//purple
             //code to change associated particle effect colours
         }
         else if(Rarity >= 801 && Rarity <= 850)
         {
-            Debug.Log("Item is Mythical!");
             itemParticles[0].startColor = new Color(255, 255, 125);//light yellow
             //code to change aprticle effect
         }
         else if(Rarity >= 851 && Rarity <= 875)
         {
-            Debug.Log("Item is Godly!");
             itemParticles[0].startColor = new Color(255, 100, 100);//red
             //code to change particle effect
         }
         else if(Rarity >= 876 && Rarity <= 950)
         {
-            Debug.Log("Item is cursed!");
             itemParticles[0].startColor = new Color(32, 32, 32);//black
             //codeto change particle effect, mimic an existing one but slightly more devious
         }
@@ -95,7 +88,6 @@ public class JM_ItemPickUp : MonoBehaviour
         {
             PlayerChild = col.gameObject.transform.GetChild(0);//get player child
             transform.parent = PlayerChild.transform.GetChild(0);//get player grandchildand make that the parent
-            Debug.Log("Parents transform position is: " + transform.parent.localPosition + "!");
             transform.position = transform.parent.position; //+ new Vector3(0.2f, 0, 0.1f);//position object at parents. seems vector mathematics and velocity don't mix at all.
             transform.rotation = transform.parent.rotation;
             mMB_WMScript.NewWeapon = this.gameObject;//tell the weapon script this is the new eapon
--- Assets/CW2/CW2 Scripts/JM_ShootScript.cs
+++ Assets/CW2/CW2 Scripts/JM_ShootScript.cs
@@ -5,19 +5,44 @@ public class JM_ShootScript : MonoBehaviour
 {
     public GameObject mPF_Bullet;//bullet prefab
     public GameObject mGO_Bullet;//bullet object colned in game
+
+    private bool Parented;
 	
 	// Update is called once per frame
 	void Update ()
     {
+        CheckParent();
         Shoot();
 	}
 
+    void CheckParent()
+    {
+        Transform Parent;
+        Transform Grandparent;
+
+        Parent = gameObject.transform.parent;
+        Grandparent = Parent.transform.parent;
+
+        if (Grandparent == null)
+        {
+            Parented = false;
+        }
+        else if (Grandparent != null)
+        {
+            Parented = true;
+        }
+    }
+
     void Shoot()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        if(Input.GetKeyDown(KeyCode.Mouse0) && Parented)
         {
-            mGO_Bullet = (GameObject) Instantiate(mPF_Bullet, transform.position, transform.rotation);//instantiate bullet
-            mGO_Bullet.GetComponent<Rigidbody>().velocity = transform.rotation * new Vector3(0,0,-50);//add velocity to instantiated bullet based on rotation.
+            mGO_Bullet = (GameObject) Instantiate(mPF_Bullet, -1 * transform.position, transform.rotation);//instantiate bullet
+            //mGO_Bullet.GetComponent<Rigidbody>().velocity = transform.rotation * new Vector3(5,0,0);//add velocity to instantiated bullet based on rotation.
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse0) && !Parented)
+        {
 
         }
     }
